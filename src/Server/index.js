22f6e304af02c380532af4f20a08574ec80ddbb4
RaftaSV <gp0555032019@unab.edu.sv>
2024-07-21 21:39:28 -0600

    Agregar ajustes de vercel 1.0

--- src/Server/index.js
+++ src/Server/index.js
@@ -1,28 +1,28 @@
-// api/index.js
 import express from 'express';
 import cors from 'cors';
+
+import config from '../config/index.js';
 import { initializeDB } from '../dataBase/index.js';
+
+const { port } = config();
 
 const app = express();
 
-// Configuración de Express
-app.use(cors());
-app.use(express.json());
+// creating Server
+const initializeServer = async (routes) => {
+  // initialize DB
+  await initializeDB();
+  app.use(cors());
+  // json parse
+  app.use(express.json());
+ 
+  // set urls
+  app.use(routes);
 
-// Exportar la función que maneja las solicitudes
-export default async (req, res) => {
-  try {
-    // Inicializa la base de datos si es necesario
-    await initializeDB();
-
-    // Maneja las solicitudes Express
-    app(req, res, (err) => {
-      if (err) {
-        res.status(500).json({ message: 'Internal Server Error' });
-      }
-    });
-  } catch (error) {
-    console.error('Error handling request:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
+  // create express app
+  app.listen(port, () => {
+    console.log(`app listening on http://localhost:${port}`);
+  });
 };
+
+export default initializeServer;
