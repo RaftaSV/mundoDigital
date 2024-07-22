9b59339dd79d10584ebe1efd79af64804cc0bd93
RaftaSV <62962896+RaftaSV@users.noreply.github.com>
2024-07-22 06:36:49 -0600

    Actualizar index.js

--- src/Server/index.js
+++ src/Server/index.js
@@ -5,18 +5,25 @@ import { initializeDB } from '../dataBase/index.js';
 
 const app = express();
 
-
 const startServer = async (routes) => {
   app.use(cors());
   app.use(express.json());
+
+  // Asignar rutas
+  routes.forEach(route => {
+    app.use(route.path, route.handler);
+  });
+
   app.get("/", (req, res) => {
     return res.status(200).json({
       data: 'hola'
-    })
+    });
   });
+
   try {
     await initializeDB();
     const { port } = config();
+    console.log(port)
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
