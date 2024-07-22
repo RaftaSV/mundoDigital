0a1fdb6369d4d71bf5fa0ea5d698d06b88c90229
RaftaSV <gp0555032019@unab.edu.sv>
2024-07-21 21:47:17 -0600

    Agregar ajustes de vercel 1.1

--- src/Server/index.js
+++ src/Server/index.js
@@ -1,28 +1,21 @@
 import express from 'express';
 import cors from 'cors';
-
-import config from '../config/index.js';
 import { initializeDB } from '../dataBase/index.js';
+import routes from '../routes/index.js';
 
-const { port } = config();
 
 const app = express();
 
-// creating Server
-const initializeServer = async (routes) => {
-  // initialize DB
-  await initializeDB();
-  app.use(cors());
-  // json parse
-  app.use(express.json());
- 
-  // set urls
+app.use(cors());
+app.use(express.json());
+
+// Inicializa la base de datos
+initializeDB().then(() => {
+  // Configura las rutas después de la inicialización de la base de datos
   app.use(routes);
+});
 
-  // create express app
-  app.listen(port, () => {
-    console.log(`app listening on http://localhost:${port}`);
-  });
+// Exporta la aplicación Express para el entorno serverless
+export default (req, res) => {
+  return app(req, res);
 };
-
-export default initializeServer;
